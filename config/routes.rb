7f0300479b1c742f20175d93722f45be32eba94d
ryouta9121 <nyagoro33@gmail.com>
2022-01-24 11:56:12 +0900

    mv files

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # get 'questions/index'
+  # get 'questions/show'
+  # get 'questions/new'
+  # get 'questions/edit'
+  # get 'questions/index'
+
+  root'questions#index'
+  resources :questions
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
